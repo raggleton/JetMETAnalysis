e418ec026f92273ab7bc3ca2d5a036a5edb5626e
Robin Aggleton <robin.aggleton@cern.ch>
2018-09-25 18:57:20 +0200

    Update batch L2 script with fit func etc

--- JetAnalyzers/output/doAllL2FlavourFits.py
+++ JetAnalyzers/output/doAllL2FlavourFits.py
@@ -10,10 +10,13 @@ infos = [
 # ("QCD_Pt_15toInf_NoJEC_newFlav", "QCD_Pt_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_nbinsrelrsp_10k/jra_QCD_Pt_15toInf_NoJEC_newFlav_ak4pfchs_L1FastJet_fineBinning_rspRangeLarge_absEta.root"),
 # ("QCD_Pt_15toInf_NoJEC_newFlav", "QCD_Pt_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_nbinsrelrsp_10k/jra_QCD_Pt_15toInf_NoJEC_newFlav_ak4pfchs_L1FastJet_wideBinning_rspRangeLarge_absEta.root"),
 # ("QCD_Pt_15toInf_NoJEC_newFlav", "QCD_Pt_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_PhysicsAlgoHadron_nbinsrelrsp_10k/jra_QCD_Pt_15toInf_NoJEC_newFlav_ak4pfchs_L1FastJet_wideBinning_rspRangeLarge_absEta_hadron.root"),
+("QCD_Pt_15toInf_NoJEC_newFlav", "QCD_Pt_NoJEC_relPtHatCut2p5_jtptmin4_withPF_PhysicsAlgoHadron_L1FastJet_Summer16_07Aug2017_V15_HadronParton_nbinsrelrsp_10k/jra_QCD_Pt_15toInf_NoJEC_newFlav_ak4pfchs_L1FastJet_rspRangeLarge_absEta_hadronParton.root"),
 
-# ("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ak4pfchs_L1FastJet_fineBinning_rspRangeLarge_absEta.root")
-# ("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ak4pfchs_L1FastJet_wideBinning_rspRangeLarge_absEta_hadron.root")
-("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_PhysicsAlgoHadron_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ak4pfchs_L1FastJet_wideBinning_rspRangeLarge_absEta_hadron.root")
+# ("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ak4pfchs_L1FastJet_fineBinning_rspRangeLarge_absEta.root"),
+# ("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ak4pfchs_L1FastJet_wideBinning_rspRangeLarge_absEta_hadron.root"),
+# ("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_PhysicsAlgoHadron_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ak4pfchs_L1FastJet_wideBinning_rspRangeLarge_absEta_hadron.root"),
+# ("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut5_jtptmin4_PhysicsAlgoHadron_L1FastJet_Summer16_07Aug2017_V11_L1fix_HadronParton_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ak4pfchs_L1FastJet_rspRangeLarge_absEta_hadronParton.root"),
+# ("QCD_Pt_15to7000_Herwig_NoJEC_newFlav", "QCD_Pt_Herwig_NoJEC_relPtHatCut2p5_jtptmin4_PhysicsAlgoHadron_L1FastJet_Summer16_07Aug2017_V15_HadronParton_nbinsrelrsp_10k/jra_QCD_Pt_15to7000_Herwig_NoJEC_newFlav_ext_ak4pfchs_L1FastJet_rspRangeLarge_absEta_hadronParton.root"),
 
 ]
 
@@ -26,17 +29,19 @@ all_algos = [
 ][:]
 
 flavours = [
-    # "ud_",
-    # "s_",
+    "ud_",
+    "s_",
     "c_",
     "b_",
-    # "g_",
-    # "",
+    "g_",
+    "",
 ]
 
 fit_min = 15
+# fit_func = "standard"
+fit_func = "powerlaw"
 
-append = "_standardMedianErr_allMedian_rspRangeLarge_fitMin%d_useFitRange_wideBinning_absEta_hadron" % fit_min
+append = "_standardMedianErr_allMedian_rspRangeLarge_fitMin%d_fitFunc%s_useFitRange_absEta_hadronParton" % (fit_min, fit_func)
 
 for name, input_file in infos:
     input_dir = os.path.dirname(input_file)
@@ -50,10 +55,11 @@ for name, input_file in infos:
                 "outputfile": "l2%s_%s.root" % (append, flav_name),
                 "algos": algo,
                 "pdgid": flav,
-                "era": "Summer16_07Aug2017_V10%s_%s" % (append, flav_name),
+                "era": "Summer16_07Aug2017_V15_%s_%s" % (append, flav_name),
                 "fitmin" : str(fit_min),
+                "fitfunc": fit_func,
             }
             args_dict  = {k.upper(): v for k, v in args_dict.items()}
-            cmd = 'jet_l2_correction_x -input "{INPUT}" -algs "{ALGOS}" -l2l3 true -histMet median -era "{ERA}" -outputDir "{OUTPUTDIR}" -output "{OUTPUTFILE}" -batch true -flavor "{PDGID}" -fitMin "{FITMIN}" -l2pffit standard'.format(**args_dict)
+            cmd = 'jet_l2_correction_x -input "{INPUT}" -algs "{ALGOS}" -l2l3 true -histMet median -era "{ERA}" -outputDir "{OUTPUTDIR}" -output "{OUTPUTFILE}" -batch true -flavor "{PDGID}" -fitMin "{FITMIN}" -l2pffit "{FITFUNC}"'.format(**args_dict)
             print cmd
             subprocess.call(cmd, shell=True)
