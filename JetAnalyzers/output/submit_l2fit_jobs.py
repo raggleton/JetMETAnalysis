e13f8e330cc52635fda32d6a937f9b14d87f89f2
Robin Aggleton <robin.aggleton@cern.ch>
2018-10-30 11:29:24 +0100

    Add second hadd stage to L2 DAG

--- JetAnalyzers/output/submit_l2fit_jobs.py
+++ JetAnalyzers/output/submit_l2fit_jobs.py
@@ -44,6 +44,14 @@ arguments = ('jet_l2_correction_x '
 job += '\narguments = "%s"\n\n' % arguments
 job += "\nqueue\n"
 
+
+hadd_job = dict_to_condor_contents(create_condor_template_dict())
+hadd_job += "\n"
+hadd_job += "JobBatchName=HaddL2\n"
+hadd_job += '\narguments = "hadd -f $(finalfile) $(inputfiles)"\n\n'
+hadd_job += "\nqueue\n"
+
+
 infos = [
 
 # ("QCD_Pt_15toInf_NoJEC_newFlav", "QCD_Pt_NoJEC_relPtHatCut5_jtptmin4_withPF_Summer16_07Aug2017_V10_nbinsrelrsp_10k/jra_QCD_Pt_15toInf_NoJEC_newFlav_ak4pfchs_L1FastJet_fineBinning_rspRangeLarge_absEta.root"),
@@ -89,10 +97,17 @@ append = "_standardMedianErr_allMedian_rspRangeLarge_fitMin%d_fitFunc%s_HadronPa
 job_args = []
 job_names = []
 
+hadd_job_names = []
+hadd_job_args = []
+hadd_job_depends = []
+
 for name, input_file in infos:
     input_dir = os.path.dirname(input_file)
 
     for algo in all_algos:
+        these_job_names = []
+        component_files = []
+
         for flav in flavours:
             flav_name = "all" if flav == "" else flav.rstrip("_")
             args_dict = {
@@ -107,26 +122,44 @@ for name, input_file in infos:
                 "fitfunc": fit_func,
             }
             job_args.append(" ".join(['%s="%s"' % (k, v) for k,v in args_dict.items()]))
-            job_names.append(args_dict['name'])
+            these_job_names.append(args_dict['name'])
+            component_files.append(os.path.join(args_dict['outputdir'], args_dict['outputfile']))
+
+        job_names.extend(these_job_names)
+        final_output_file = os.path.join(input_dir, "l2%s.root" % append)
+        hadd_name = "HADD_%s_%s" % (name, algo)
+        hadd_job_args.append('name="%s" finalfile="%s" inputfiles="%s"' % (hadd_name, final_output_file, " ".join(component_files)))
+        hadd_job_depends.append(' '.join(these_job_names))
+        hadd_job_names.append(hadd_name)
 
 
 if len(job_names) == 0:
     raise RuntimeError("Didn't find any files to run over!")
 
-job_filename = "htc_do_jet_l2_correction_x_job.condor"
-with open(job_filename, 'w') as f:
+htc_filename = "htc_do_jet_l2_correction_x_job.condor"
+with open(htc_filename, 'w') as f:
     f.write(job)
 
 dag = ""
 for ind, (job_name, job_args_entry) in enumerate(zip(job_names, job_args)):
-    dag += "JOB {0} {1}\n".format(job_name, job_filename)
+    dag += "JOB {0} {1}\n".format(job_name, htc_filename)
     dag += "VARS {0} {1}\n".format(job_name, job_args_entry)
 
-dag += "RETRY ALL_NODES 3\n"
-status_file = job_filename.replace(".condor", ".dagstatus")
+# Add hadd jobs
+hadd_htc_filename = "hadd_%s" % htc_filename
+with open(hadd_htc_filename, 'w') as f:
+    f.write(hadd_job)
+
+for ind, (hname, harg, hdeps) in enumerate(zip(hadd_job_names, hadd_job_args, hadd_job_depends)):
+    dag += "JOB {0} {1}\n".format(hname, hadd_htc_filename)
+    dag += "VARS {0} {1}\n".format(hname, harg)
+    dag += "PARENT {0} CHILD {1}\n".format(hdeps, hname)
+
+# dag += "RETRY ALL_NODES 3\n"  # dont want this as l2 purposely fails when doing checkFormulaEvaluator
+status_file = htc_filename.replace(".condor", ".dagstatus")
 dag += "NODE_STATUS_FILE %s\n" % (status_file)
 
-dag_filename = job_filename.replace(".condor", ".dag")
+dag_filename = htc_filename.replace(".condor", ".dag")
 with open(dag_filename, 'w') as f:
     f.write(dag)
 
