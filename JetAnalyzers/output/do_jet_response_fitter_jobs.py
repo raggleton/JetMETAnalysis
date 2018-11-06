0090e8cd2bedbc7a704fa4ac8c0adedb1018a7fc
Robin Aggleton <robin.aggleton@cern.ch>
2018-11-06 10:33:41 +0100

    remove output file if already exists

--- JetAnalyzers/output/do_jet_response_fitter_jobs.py
+++ JetAnalyzers/output/do_jet_response_fitter_jobs.py
@@ -21,11 +21,14 @@ append = "L1FastJet_wideBinning_rspRangeLarge_absEta_physicsParton_rebin%d.root"
 for name, input_file in infos:
     output_dir = os.path.dirname(input_file)
     for algo in all_algos:
+        output_filename = os.path.join(output_dir, "jrf_%s_%s_%s" % (name, algo, append))
         args_dict = {
             "inputf": input_file,
-            "outputf": os.path.join(output_dir, "jrf_%s_%s_%s" % (name, algo, append)),
+            "outputf": output_filename,
             "algos": algo,
             "rebin": str(rebin),
         }
+        if os.path.isfile(output_filename):
+            os.remove(output_filename)
         cmd = "jet_response_fitter_x -input {inputf} -output {outputf} -algs {algos} -doFlavor true -rebin {rebin}".format(**args_dict)
         subprocess.check_call(cmd, shell=True)
